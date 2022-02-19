cf83fb73c5cab349006801d79e195ff33a075655
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2022-02-18 21:40:40 -0800

    Added expirable-storage

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import * as glize from "glize";
-import {HamburgerDrawer} from 'react-hamburger-drawer';
+import { HamburgerDrawer } from "react-hamburger-drawer";
+import {
+  expirableLocalStorage,
+  expirableSessionStorage,
+} from "expirable-storage";
 
-export {
-  glize,
-  HamburgerDrawer
-};
+export { glize, HamburgerDrawer };
+export { expirableLocalStorage, expirableSessionStorage };
